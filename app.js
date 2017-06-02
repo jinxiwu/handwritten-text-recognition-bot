901c4abb378d54671b953768366f3b33aec41165
jinxiwu <jinxwu@microsoft.com>
2017-06-02 14:11:24 -0700

    Change port from 81 to 443 for https

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var builder = require('botbuilder'),
 
 // Bot Setup
 var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 81,
+server.listen(process.env.port || process.env.PORT || 443,
     function() {
         console.log('%s listening to %s', server.name, server.url);
     });
